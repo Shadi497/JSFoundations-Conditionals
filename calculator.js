a6e8909e0996ebf7cdfcd14063d920457a25eafd
Shadi497 <b.shadi.h@gmail.com>
2021-01-10 15:44:15 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,23 @@
-// Define the numbers and the operation here
+let numone = 10;
+let numtwo = 15;
+let oper = "addition";
 
-// Write if-statements here
+if(oper==="addition"){
+  let result= numone+numtwo;
+  console.log(`Your result is ${result}`);
+}
+else if(oper.toLowerCase==="subtraction"){
+  let result=numone-numtwo;
+  console.log(`Your result is ${result}`);
+}
+else if(oper.toLowerCase==="multiplication"){
+  let result=numone*numtwo;
+  console.log(`Your result is ${result}`);
+}
+else if(oper.toLowerCase==="division" && numtwo !== 0 ){
+  let result=numone/numtwo;
+  console.log(`Your result is ${result}`);
+}
+else{
+  console.log(`Please check your input`);
+}
